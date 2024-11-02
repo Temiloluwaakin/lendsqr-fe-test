f43567d8056a8f94f710c1bd2e2cec01ea9a730b
Temiloluwaakin <106915700+Temiloluwaakin@users.noreply.github.com>
2024-11-03 00:43:23 +0100

    layout change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -60,10 +60,35 @@ const Header: React.FC<HeaderProps>  = ({onHamburgerClick}) => {
 
     return (
         <header className="header">
-            <h1>My App</h1>
             <div className="hamburger" onClick={onHamburgerClick}>
                 ☰
             </div>
+
+            <div className='nav-logo'>
+                <Link to='/'>
+                    <img src={logo} alt="logo" />
+                </Link>
+            </div>
+
+            <div className='search'>
+                <input placeholder='search for anything'/>
+                <div className='search-icon'> 
+                <img src={search} alt='search-icon'/> 
+                </div>
+            </div>
+
+            <div className='right-side-header'>
+                <Link to='/'> <p>Docs</p> </Link>
+                <Link to='/'>
+                    <img src={notification} className="notification-icon" alt="notification-icon" />
+                </Link>
+                <Link to='/'>
+                    <img src={avatar} alt="avatar" className='avatar' />
+                </Link>
+                <p>Ayodeji</p>
+                <img src={dropdown} alt='dropdown-caret' className='dropdown-caret'/>
+            </div>
+            
             <div className="dropdown-container">
                 <button onClick={toggleDropdown} className="dropdown-toggle">
                 Menu ▼
